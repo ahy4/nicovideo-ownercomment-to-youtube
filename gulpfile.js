40a99b87d3c93b8023d2dda5f09f89000bbc17f8
ahyahya <mrfool38@gmail.com>
2017-02-05 00:06:36 +0900

    sbv to srt

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,32 @@ const gulp = require('gulp');
 const replace = require('gulp-replace');
 const rename = require('gulp-rename');
 
-gulp.task('default', () => {
-  gulp.src(['src/*', '!src/.gitkeep'])
-    .pipe(replace(/^(\d*[.]\d*):([^:]*):(.*)/gm, (match, second, command, message) => {
-      const start = computeDuration(Number(second)*1000);
-      const end = computeDuration((Number(second)+3)*1000);
-      return (
-        `${start},${end}` + "\n" +
-        message + "\n"
-      );
-    }))
-    .pipe(rename({
-      extname: ".sbv"
-    }))
-    .pipe(gulp.dest('./dist'));
-});
-
-
-/**
- * ミリ秒を時分秒へ変換
- */
-function computeDuration(ms) {
+const computeDuration = (ms) => { // [ミリ秒]を[時:分:秒,ミリ秒]に変換
   var h = String(Math.floor(ms / 3600000) + 100).substring(1);
   var m = String(Math.floor((ms - h * 3600000)/60000)+ 100).substring(1);
   var s = String(Math.floor((ms - h * 3600000 - m * 60000)/1000)+ 100).substring(1);
   var ms = String(ms % 1000);
-  return h+':'+m+':'+s+'.'+ms;
-}
+  return h+':'+m+':'+s+','+ms;
+};
+
+const replacer = (function () {
+  let i = 1;
+  return (match, second, command, message) => {
+    const start = computeDuration(Number(second)*1000);
+    const end = computeDuration((Number(second)+3)*1000);
+    const parsedLines = (
+      `${i++}\n` +
+      `${start} --> ${end}\n` +
+      message + "\n"
+    );
+    return parsedLines;
+  };
+}());
+
+gulp.task('default', () => {
+  gulp.src(['src/*', '!src/.gitkeep'])
+    .pipe(replace(/^(\d*[.]\d*):([^:]*):(.*)/gm, replacer))
+    .pipe(rename({ extname: ".srt" }))
+    .pipe(gulp.dest('./dist'));
+});
+
